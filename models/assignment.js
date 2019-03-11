d240c9f73bb885b1a6e1fb7f5f82474ca5048fea
Bryant Gossett <5419632+Bgosse1@users.noreply.github.com>
2019-03-10 20:48:01 -0400

    updated controller

--- models/assignment.js
+++ models/assignment.js
@@ -4,13 +4,13 @@ const Schema = mongoose.Schema;
 const assignmentSchema = new Schema({
   assignmentName: { type: String, required: true },
   type: { type: String },
-  completed: { type: [String], required: true },
-  dueDate: { type: String, required: true },
-  isRequired: { type: String, required: true },
+  completed: { type: Boolean, required: true },
+  dueDate: { type: Date, required: true },
+  isRequired: { type: Boolean, required: true },
   assignmentDetails: { type: String, required: true },
-  assignmentLink: { type: String, required: true, unique: true }
+  assignmentLink: { type: String, required: true }
 });
 
-const Assignment = mongoose.model("Book", assignmentSchema);
+const Assignment = mongoose.model("Assignment", assignmentSchema);
 
 module.exports = Assignment;
