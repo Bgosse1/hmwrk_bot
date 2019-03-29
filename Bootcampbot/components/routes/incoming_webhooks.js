0504812fef7b2277acc202a42de685c270f3a3e7
Bryant Gossett <5419632+Bgosse1@users.noreply.github.com>
2019-03-29 09:14:33 -0400

    wake up bot  button

--- Bootcampbot/components/routes/incoming_webhooks.js
+++ Bootcampbot/components/routes/incoming_webhooks.js
@@ -3,7 +3,6 @@ var debug = require('debug')('botkit:incoming_webhooks');
 module.exports = function(webserver, controller) {
     debug('Configured /slack/receive url');
     webserver.post('/slack/receive', function(req, res) {
-
         // NOTE: we should enforce the token check here
 
         // respond to Slack that the webhook has been received.
